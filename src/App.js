06e655e22cb5684c4ab27b34e80b64c3032b49ab
Baltazar Alvarez <baltazaralvarez@gmail.com>
2021-04-24 14:56:32 -0300

    added header componenet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePAge from "./pages/homepage/homepage.component";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.componenet";
 
 function App() {
   return (
       <div>
-        <Route exact path='/' component={HomePAge} />
-        <Route exact path='/shop' component={ShopPage} />
+          <Header/>
+          <Switch>
+              <Route exact path='/' component={HomePAge} />
+              <Route exact path='/shop' component={ShopPage} />
+          </Switch>
       </div>
   );
 }

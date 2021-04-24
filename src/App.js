643572700d03277e46e16f7435c44d83547e68b9
Baltazar Alvarez <baltazaralvarez@gmail.com>
2021-04-24 02:35:53 -0300

    added first routing to hats page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePAge from "./pages/homepage/homepage.component";
+import {Route} from "react-router-dom";
+
+const HatsPage = () => (
+    <div>
+       <h1>
+           HATS PAGE!
+       </h1>
+    </div>
+)
 
 function App() {
   return (
-    <HomePAge />
+      <div>
+        <Route exact path='/' component={HomePAge} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </div>
   );
 }
 
